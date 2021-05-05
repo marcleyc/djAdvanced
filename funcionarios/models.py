107d7f3246a8ba9a0665172cabe729de6a3a14e0
marcleyc <marcleycc@gmail.com>
2021-05-05 06:26:08 +0100

    Tudo 100%

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from departamentos.models import Departamento
+from empresas.models import Empresas
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresas, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
-
-
